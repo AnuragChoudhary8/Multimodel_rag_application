05eb5da1038b99fb233109b494f2e9ee3436a76a
AnuragChoudhary8 <142927929+AnuragChoudhary8@users.noreply.github.com>
2025-05-18 17:49:26 +0530

    pdf_parser.py

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -1,4 +1,11 @@
 from unstructured.partition.pdf import partition_pdf
 
-def extract_elements_from_pdf(path: str):
-    return partition_pdf(filename=path)
+def extract_elements_from_pdf(path: str, output_dir="extracted_data"):
+    return partition_pdf(
+        filename=path,
+        strategy="hi_res",
+        extract_images_in_pdf=True,
+        extract_image_block_types=["Image", "Table"],
+        extract_image_block_output_dir=output_dir,
+        extract_image_block_to_payload=False
+    )
